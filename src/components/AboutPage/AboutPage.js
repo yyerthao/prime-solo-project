43e7f6db0f6f2c82f99848b74a80bb21c7f6da1b
Yer Thao <maivyerthao@gmail.com>
2021-01-15 10:51:39 -0600

    Update About page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -12,12 +12,21 @@ const AboutPage = () => (
     <Nav/>
     <div>
       <h1>About</h1>
-      <p>Dreams are an intriguing thing.</p>
-      <p>Dreams are an intriguing thing.</p>
-      <p>Dreams are an intriguing thing.</p>
-      <p>Dreams are an intriguing thing.</p>
-      <p>Dreams are an intriguing thing.</p>
-      <p>Dreams are an intriguing thing.</p>
+      <p>
+      The human mind always ticks. It interprets things in our wake and
+      </p>
+      <p>
+      it interprets things while we sleep. 
+      This process is called dreaming. 
+      </p>
+      <p>
+      While we sleep, our mind attempts to make sense of things we may have 
+      </p>
+      <p>
+      encountered, things we simply looked at without a second thought on.
+      </p>
+
+
     </div>
   </div>
 );
