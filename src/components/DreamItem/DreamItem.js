7b33cbdf8862d8adc60e806b0982864d43e7c3b2
Yer Thao <maivyerthao@gmail.com>
2021-01-16 21:00:00 -0600

    Added in sweet alerts and buttons to dreamItem view

--- src/components/DreamItem/DreamItem.js
+++ src/components/DreamItem/DreamItem.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import Nav from '../Nav/Nav';
+import Swal from 'sweetalert2';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
 // component.
 class DreamItem extends Component {
-  state = {
-    // heading: 'Class Component',
-
-  };
 
 
+  deleteDream = () => {
+    console.log('Deleting dream')
+    // will be dispatching action for delete route here
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your dream has been deleted.',
+          'Success'
+        )
+      }
+    })
+  }
+
+  updateDream = () => {
+    console.log('Updating dream')
+    // will be dispatching action here for PUT route
+  }
 
 
   render() {
@@ -28,7 +51,7 @@ class DreamItem extends Component {
         {details.map((details, i) => {
           return (
             <div key={i}>
-          <h4>Title: {details.title}</h4>
+          <h2>Title: {details.title}</h2>
           <h5>Date: {details.to_char}</h5>
           <img src={details.image} alt="Dream"></img>
           <h5>Genre: {details.name}</h5>
@@ -36,6 +59,10 @@ class DreamItem extends Component {
             </div>
           )
         })}
+
+
+          <button className="delete-button" onClick={this.deleteDream}>Delete</button>
+          <button className="update-button" onClick={this.updateDream}>Update</button>
         </div>
 
       </div>
